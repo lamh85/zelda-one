b919db08789b6713e8ae47f654237fa80505be83
lamh85 <lamh85@gmail.com>
2025-01-02 21:05:00 -0800

    set movement boundaries

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ let isRightPressed = false
 let isDownPressed = false
 let isLeftPressed = false
 
+const PLAYER_HEIGHT = 50
+const PLAYER_WIDTH = 50
+const SCREEN_WIDTH = 1000
+const SCREEN_HEIGHT = 700
+
 const isKeyPressed = {
   ArrowUp: false,
   ArrowRight: false,
@@ -24,6 +29,30 @@ htmlTag.addEventListener('keyup', (event) => {
 
 const playerCharacter = document.querySelector('#player')
 
+const validatePosition = (requestedPosition, direction) => {
+  if (requestedPosition < 0) {
+    return 0
+  }
+
+  if (direction === 'right') {
+    const rightPosition = requestedPosition + PLAYER_WIDTH
+
+    if (rightPosition > SCREEN_WIDTH) {
+      return SCREEN_WIDTH - PLAYER_WIDTH
+    }
+  }
+
+  if (direction === 'down') {
+    const downPosition = requestedPosition + PLAYER_HEIGHT
+
+    if (downPosition > SCREEN_HEIGHT) {
+      return SCREEN_HEIGHT - PLAYER_HEIGHT
+    }
+  }
+
+  return requestedPosition
+}
+
 const moveCharacter = (direction) => {
   if (!['left', 'right', 'up', 'down'].includes(direction)) {
     return
@@ -33,10 +62,12 @@ const moveCharacter = (direction) => {
   const distanceFactor = ['left', 'up'].includes(direction) ? '-1' : 1
 
   const currentPosition = playerCharacter.getAttribute(axis)
-  playerCharacter.setAttribute(
-    axis,
-    parseInt(currentPosition) + distanceFactor * 10
+  const newPosition = validatePosition(
+    parseInt(currentPosition) + distanceFactor * 10,
+    direction
   )
+
+  playerCharacter.setAttribute(axis, newPosition)
 }
 
 const detectKeyPressOnTick = () => {
